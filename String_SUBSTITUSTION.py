deb5e53fb8ee2d0d03ee699c2954d878830d35c0
Emmett Shang <emmett.shang@gmail.com>
2018-04-18 14:27:42 +1000

    First version

---
+++ String_SUBSTITUSTION.py
@@ -0,0 +1,17 @@
+#TEST_MESSAGE = input()
+substitution = [['a', '4'], ['e', '3'], ['l', '1'], ['o', '0'], ['t', '7']]
+def encode_message(message, substitution):
+    for s in substitution:
+        old = s[0]
+        new = s[1]
+        converted = message.replace(old, new)
+        message = converted
+        print("converted text = "+converted)
+        print(s)
+    return converted
+message = input("Type the message to be encoded here: ")
+converted_text = encode_message(message, substitution)
+print("started with "+message)
+print("Converted to "+converted_text)
+
+
