70992158db28475d8df3b1ef8aade04870d516ac
Emmett Shang <emmett.shang@gmail.com>
2018-05-20 19:42:48 +1000

    operators

---
+++ basic_arithimetic_operators.py
@@ -0,0 +1,23 @@
+x = 13
+y = 42
+
+if x == y:
+    print("True")
+else:
+    print("False")
+if x < y:
+    print("True")
+else:
+    print("False")
+if x > y:
+    print("True")
+else:
+    print("False")
+if x <= y:
+    print("True")
+else:
+    print("False")
+if x >= y:
+    print("True")
+else:
+    print("False")
