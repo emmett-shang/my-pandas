84f156767266c0fe4b7bd6a469ceb94f0b8ea93a
Emmett Shang <emmett.shang@gmail.com>
2018-05-22 21:30:20 +1000

    another variable type

--- variable_types.py
+++ variable_types.py
@@ -16,3 +16,11 @@ print(str[2:5])  # Prints characters starting from 3rd to 5th
 print(str[2:])  # Prints string starting from 3rd character
 print(str * 2)  # Prints string two times
 print(str + "TEST")
+
+counter = 100  # An integer assignment
+miles = 1000.0  # A floating point
+name = "John"  # A string
+
+print(counter)
+print(miles)
+print(name)
