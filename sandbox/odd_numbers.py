a5e60fd0910a54c6b8d15a74732b946dcb2f6ac0
Emmett Shang <emmett.shang@gmail.com>
2018-05-26 19:56:40 +1000

    added reference comment

--- sandbox/odd_numbers.py
+++ sandbox/odd_numbers.py
@@ -1,3 +1,7 @@
+"""
+# https://www.datacamp.com/community/tutorials/python-list-comprehension
+"""
+
 # print odd numbers from 0 to 20
 print("This is the odd number list from 0 to 20:")
 numbers = range(0, 20)
