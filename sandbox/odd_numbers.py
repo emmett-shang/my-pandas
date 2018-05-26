ff850014e963953f580c370e0ea14b21090a6196
Emmett Shang <emmett.shang@gmail.com>
2018-05-26 19:51:00 +1000

    even numbers from 30 to 50

--- sandbox/odd_numbers.py
+++ sandbox/odd_numbers.py
@@ -1,4 +1,13 @@
-numbers = range(0,20)
+# print odd numbers from 0 to 20
+print("This is the odd number list from 0 to 20:")
+numbers = range(0, 20)
 for n in numbers:
     if n % 2 != 0:
         print(n)
+
+# print event numbers from 30 to 50
+print("This is the even number list from 30 to 50:")
+numbers_E = range(30, 51)
+for nE in numbers_E:
+    if nE % 2 == 0:
+        print(nE)
