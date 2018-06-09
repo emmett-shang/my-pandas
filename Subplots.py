4e5c2820f98a657847f21e7de0eda14e90f46987
Emmett Shang <emmett.shang@gmail.com>
2018-06-09 20:37:31 +1000

    a substitute plot

---
+++ Subplots.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Compute the x and y coordinates for points on sine and cosine curves
+x = np.arange(0, 3 * np.pi, 0.1)
+y_sin = np.sin(x)
+y_cos = np.cos(x)
+y_mbo = np.cos(x)
+y_egg = np.cos(x)
+# Set up a subplot grid that has height 2 and width 1,
+# and set the first such subplot as active.
+plt.subplot(2, 2, 1)
+
+# Make the first plot
+plt.plot(x, y_sin)
+plt.title('Sine')
+
+# Set the second subplot as active, and make the second plot.
+plt.subplot(2, 2, 2)
+plt.plot(x, y_cos)
+plt.title('Cosine')
+
+plt.subplot(2, 2, 3)
+plt.plot(x, y_mbo)
+plt.title('mbo')
+
+plt.subplot(2, 2, 4)
+plt.plot(x, y_egg)
+plt.title('egg')
+# Show the figure.
+plt.show()
