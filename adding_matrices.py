bebd4faf0774a843fee0b39db4fecaca42ce2d88
Emmett Shang <emmett.shang@gmail.com>
2018-06-13 22:01:56 +1000

    adding matrices

---
+++ adding_matrices.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+v = np.array([[1],
+              [2],
+              [3]])
+
+u = np.array([[4],
+              [5],
+              [6]])
+print(v + v)
+#[[2]
+ #[4]
+ #[6]]
+
+print(3 * v)
+#[[3]
+ #[6]
+ #[9]]
+
+print(u + u)
+
+print(3 * u)
+
+print(v + u)
+
+print(v * u)
