92a9e0682bb4f16e123e7af1df1ea31f842e83f4
Emmett Shang <emmett.shang@gmail.com>
2018-06-17 21:57:40 +1000

    array math

--- array_math.py
+++ array_math.py
@@ -7,7 +7,10 @@ x = np.array([
     [1, 2],
     [3, 4]
 ], dtype=np.float64)
-y = np.array([[5, 6], [7, 8]], dtype=np.float64)
+y = np.array([
+    [5, 6],
+    [7, 8]
+], dtype=np.float64)
 
 print(x)
 print(y)
