883047cc0a70e9e7c4452d2ddfc56fe906e9ab1f
Emmett Shang <emmett.shang@gmail.com>
2018-06-18 22:27:03 +1000

    3 operations using matrices, multiplication, addition and subtraction

---
+++ matrix_operations.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+"""
+
+https://www.youtube.com/watch?v=TeQOY4aJCLc
+"""
+a = np.array([
+    [1, 1, 2],
+    [3, 5, 8]
+])
+
+b = np.array([
+    [2, 3, 5],
+    [7, 11, 13],
+])
+
+print(a + b)
+
+c = np.array([
+    [3, 1, 4],
+    [1, 5, 9],
+    [2, 6, 5],
+    [3, 5, 6]
+])
+print(c * 100)
+print(c * -100)
