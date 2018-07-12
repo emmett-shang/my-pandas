a059b2e9aa5a455e3fc08cc07a4577196f44d08d
Emmett Shang <emmett.shang@gmail.com>
2018-07-12 22:46:49 +1000

    another way to do a histogram

--- Frequency Histogram.py
+++ Frequency Histogram.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import random
 
 data = [3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10]
@@ -14,13 +15,8 @@ plt.show()
 # Head: 1Frequency Histogram.py:18
 # Tail: 0
 
-
-coin_toss = []
-
-for i in range(50000):
-    toss = random.randint(0, 1)
-    coin_toss.append(toss)
-    print(toss)
+frac = 0.5
+coin_toss = np.random.binomial(1, frac, size=50000)
 
 plt.hist(coin_toss, bins=10, alpha=0.5, normed=False, color='y', range=[0, 1], align='left')
 
