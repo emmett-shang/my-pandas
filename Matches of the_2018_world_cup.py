bf0b0670228150eca1ef0f98b378ad5121eeb14d
Emmett Shang <emmett.shang@gmail.com>
2018-07-19 22:10:08 +1000

    printing out the soccer matches

---
+++ Matches of the_2018_world_cup.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import os
+
+"""
+https://docs.python.org/3.5/library/os.path.html
+
+"""
+
+path = r"./data"
+path = os.path.abspath(path)
+
+file_name = '2018_world_cup_matches.csv'
+full_file_name = os.path.join(path, file_name)
+
+df = pd.read_csv(full_file_name)
+print(df)
