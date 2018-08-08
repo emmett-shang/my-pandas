835e8b16bcbac18bf9b672b60b39744bd74f6b76
Emmett Shang <emmett.shang@gmail.com>
2018-08-08 22:19:01 +1000

    random soccer players

---
+++ random_soccer_players.py
@@ -0,0 +1,9 @@
+from random import *
+
+top_5_best_soccer_players_of_all_time = ['Messi', 'Pele', 'Maradona', 'Ronadinio', 'Ronaldo']
+
+x = sample(top_5_best_soccer_players_of_all_time, 1)
+print x[0]
+
+y = sample(top_5_best_soccer_players_of_all_time, 2)
+print y
