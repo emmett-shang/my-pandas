0cc4a5148ff284256e7f12115d7b87f2479b619c
Emmett Shang <emmett.shang@gmail.com>
2018-08-20 21:21:36 +1000

    Soccer players

--- list_index.py
+++ list_index.py
@@ -3,9 +3,25 @@ index = random.index('Messi')
 print("The index of Messi is:", index)
 
 random.append('Zidane')
-print('Updated animal list: ', random)
+print('Updated list: ', random)
 
-random_2 = ['Modric', 'Griezemann', 'Neymar ', 'Salah', 'Suarez', 'Sanchez']
+random_2 = ['Modric', 'Griezemann', 'Neymar ', 'Salah', 'Suarez','Kane', 'Hazard']
 
 random.extend(random_2)
 print('random list:', random)
+
+random.insert(7, 'Sanchez')
+print('Updated', random)
+
+random.remove('Kane')
+print('Updated list', random)
+
+count = random.count('Ronaldinho')
+print('The count of Ronaldinho is:', count)
+count = random.count('Cavani')
+print('The count of Cavani is:', count)
+
+Return_name = random.pop(12)
+print('Updated list:', random)
+
+
