241785afb8e12007f53b451ac4f1c36d6cc2d1f9
Emmett Shang <emmett.shang@gmail.com>
2018-12-28 23:11:24 +1100

    I did it

---
+++ sandbox/iterating_over_dictionary.py
@@ -0,0 +1,8 @@
+D1 = {1:'a', 2:'b', 3:'c'}
+for k in D1.keys():
+    print(k, D1[k])
+
+
+family_members = {'Dad':'Darth Vader', 'Mom':'padme Amidala ', 'Myself':'Luke Skywalker'}
+for k in family_members.keys():
+    print(k, family_members[k])
