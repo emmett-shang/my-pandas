29600ef30fbd1c95901830e5839dbff2c92db17f
Emmett Shang <emmett.shang@gmail.com>
2019-01-02 21:24:22 +1100

    I used the read function

--- sandbox/file_handling.py
+++ sandbox/file_handling.py
@@ -7,8 +7,14 @@ def main():
     f.close()
     f = open("File_made_by_file_handling.txt", "a+")
     for i in range(2):
-        f.write("Append line %d\r\n" % (i+1))
+        f.write("Append line %d\r\n" % (i + 1))
 
+    f = open("File_made_by_file_handling.txt", "r")
+    if f.mode == 'r':
+        contents = f.read()
+        print(contents)
     f.close()
+
+
 if __name__ == "__main__":
     main()
