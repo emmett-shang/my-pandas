67bc16d4de602f2432e91c7ac5de981d557c4303
Emmett Shang <emmett.shang@gmail.com>
2019-01-03 21:43:17 +1100

    exponential and logarithmic functions

---
+++ sandbox/index_laws.py
@@ -0,0 +1,14 @@
+Ques_1 = 4**(-2)
+solu_1 = (1/2**4)
+print(Ques_1)
+print(float(solu_1))
+
+Ques_2 = (1/2**(-3))
+solu_2 = 2**3
+print(Ques_2)
+print(float(solu_2))
+
+Ques3 = ((3)**(-2)*(6)**(3)*(12)**(-2))/((9)**(-3)*(2)**(-1))
+solu_3 = 3**5
+print(Ques3)
+print(float(solu_3))
