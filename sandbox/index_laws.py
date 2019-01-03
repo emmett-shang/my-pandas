d95dc9c5ce059660f4b276e6da18c0dc59cdfcb5
Emmett Shang <emmett.shang@gmail.com>
2019-01-03 21:48:34 +1100

    exponential and logarithmic functions

--- sandbox/index_laws.py
+++ sandbox/index_laws.py
@@ -1,16 +1,16 @@
-Ques_1 = 4**(-2)
-solu_1 = (1/2**4)
+Ques_1 = 4 ** (-2)
+solu_1 = (1 / 2 ** 4)
 print(Ques_1)
 print(float(solu_1))
 
-Ques_2 = (1/2**(-3))
-solu_2 = 2**3
+Ques_2 = (1 / 2 ** (-3))
+solu_2 = 2 ** 3
 print(Ques_2)
 print(float(solu_2))
 
-Ques3 = ((3)**(-2)*(6)**(3)*(12)**(-2))/((9)**(-3)*(2)**(-1))
-solu_3 = 3**5
+Ques3 = ((3) ** (-2) * (6) ** (3) * (12) ** (-2)) / ((9) ** (-3) * (2) ** (-1))
+solu_3 = 3 ** 5
 print(Ques3)
 print(float(solu_3))
 
-#TODO: 'use assert for ques == solu' - Daddy
+# TODO: 'use assert for ques == solu' - Daddy
