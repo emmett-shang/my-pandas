f3912b6a2c361d6da148add28990aa801ad3bd22
Emmett Shang <emmett.shang@gmail.com>
2019-01-05 22:22:42 +1100

    easier to read and code reformated

--- sandbox/read_a_file_line_by_line.py
+++ sandbox/read_a_file_line_by_line.py
@@ -1,10 +1,9 @@
 def main():
-
-    f = open('text_written_line_by_line.txt','w+')
+    f = open('text_written_line_by_line.txt', 'w+')
     for i in range(10):
         f.write("%d\r\n" % (i + 1))
 
-    f = open('text_written_line_by_line.txt','r')
+    f = open('text_written_line_by_line.txt', 'r')
     f1 = f.readlines()
     for x in f1:
         print(x)
