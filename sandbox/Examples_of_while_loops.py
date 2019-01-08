449e0f0c13d999a7b38c21d5a523a658988c3ba9
Emmett Shang <emmett.shang@gmail.com>
2019-01-08 14:20:11 +1100

    3 examples of while loops

---
+++ sandbox/Examples_of_while_loops.py
@@ -0,0 +1,23 @@
+#The while loop
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+
+
+#The break statement
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
+
+
+#The continue statement
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
