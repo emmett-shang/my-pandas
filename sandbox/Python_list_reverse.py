9156dc9fb01a94880dd0f8079b2860b9914416ba
Emmett Shang <emmett.shang@gmail.com>
2019-01-17 17:21:38 +1100

    elements in a list reversed

---
+++ sandbox/Python_list_reverse.py
@@ -0,0 +1,14 @@
+# Reverse a list:
+
+programming_languages = ['Python', 'Javascript', 'R']
+print('Original list:', programming_languages)
+
+programming_languages.reverse()
+
+print('Updated list:', programming_languages)
+
+# Individual elements in reversed order:
+
+programming_languages = ['Python', 'Javascript', 'R']
+for pl in reversed(programming_languages):
+    print(pl)
