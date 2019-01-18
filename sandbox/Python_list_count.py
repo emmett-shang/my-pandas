303b9acafef1fc4daeba2d6c49a881d2853304a3
Emmett Shang <emmett.shang@gmail.com>
2019-01-18 17:09:29 +1100

    list count

---
+++ sandbox/Python_list_count.py
@@ -0,0 +1,12 @@
+vowels = ['a', 'e', 'i', 'o', 'u', 'a', 'e', 'i', 'u']
+count = vowels.count('e')
+print('The count of e is:', count)
+
+count = vowels.count('o')
+print('The count of o is:', count)
+
+count = vowels.count('x')
+print('The count of x is:', count)
+
+count = vowels.count('i')
+print('The count of i is:', count)
