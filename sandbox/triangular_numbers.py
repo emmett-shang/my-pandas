65c39608442c448aa811fcd73ffd1896ea00a411
Emmett Shang <emmett.shang@gmail.com>
2019-01-29 15:59:28 +1100

    generating triangular numbers

---
+++ sandbox/triangular_numbers.py
@@ -0,0 +1,5 @@
+n = int(input("Type a number between 1 and 1,000,000,000"))
+
+triangularNumber = int(n * (n + 1) / 2)
+
+print("The " + str(n) + "th triangular number is: " + str(triangularNumber))
