fb1c53e9616349e114a3a4551490bb0a445ac698
Emmett Shang <emmett.shang@gmail.com>
2019-02-23 14:53:12 +1100

    lists

---
+++ sandbox/lists.py
@@ -0,0 +1,19 @@
+numbers = [2, 4, 6, 8]
+print(numbers[0])
+print(numbers[1])
+print(numbers[-1])
+numbers.append(10)
+print(numbers)
+x = [12, 14, 16]
+print(numbers + x)
+numbers = [1, 3, 5, 7, 11, 13, 17, 19, 23]
+numbers.reverse()
+print(numbers)
+names = ["Zofia", "Alex", "Morgan", "Anthony"]
+names.sort()
+print(names)
+names.reverse()
+print(names)
+sorted_names = sorted(names)
+print(names)
+print(sorted_names)
