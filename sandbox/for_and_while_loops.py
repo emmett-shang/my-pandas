4957a9e44da28b32e946ee1859048ab02ea7063d
Emmett Shang <emmett.shang@gmail.com>
2019-04-24 18:33:00 +1000

    repeat

---
+++ sandbox/for_and_while_loops.py
@@ -0,0 +1,15 @@
+for i in range(0, 10):
+    print(i)
+
+names = ['Jim', 'Tom', "Nick", "Pam", "Sam", "Tim"]
+for name in names:
+    print(name)
+
+for i in range(len(names)):
+    print(names[i])
+
+age = {"Jim: 31", "Nick: 31", "Pam: 27", "Sam: 35", "Tim: 31", "Tom: 50"}
+print(age)
+
+for name in age:
+    print(name, name)
