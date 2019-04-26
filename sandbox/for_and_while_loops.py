01e5afc7f2e6c4686723addae56d53520fe0cfed
Emmett Shang <emmett.shang@gmail.com>
2019-04-26 13:02:39 +1000

    keys

--- sandbox/for_and_while_loops.py
+++ sandbox/for_and_while_loops.py
@@ -8,8 +8,19 @@ for name in names:
 for i in range(len(names)):
     print(names[i])
 
-age = {"Jim: 31", "Nick: 31", "Pam: 27", "Sam: 35", "Tim: 31", "Tom: 50"}
+age = {"Jim":"31", "Nick": "31", "Pam": "27", "Sam": "35", "Tim": "31", "Tom": "50"}
 print(age)
 
+print(age.keys())
+
+for name in age.keys():
+    print(name, age[name])
+
 for name in age:
-    print(name, name)
+    print(name, age[name])
+
+for name in sorted(age.keys()):
+    print(name, age[name])
+
+for name in sorted(age.keys(), reverse=True):
+    print(name, age[name])
