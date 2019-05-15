f3ab9549f3485f235a8a62566b4a4bf803a429ec
Emmett Shang <emmett.shang@gmail.com>
2019-05-15 22:25:55 +1000

    Another circle/oval

--- sandbox/Drawing_circles_using_Matplotlib.py
+++ sandbox/Drawing_circles_using_Matplotlib.py
@@ -7,10 +7,21 @@ https://github.com/hadrienj/deepLearningBook-Notes/blob/master/2.8%20Singular%20
 
 """
 
-x = np.linspace(-1, 1, 5)
+
+x = np.linspace(-1, 1, 100000)
 y = np.sqrt(1 - (x ** 2))
 plt.plot(x, y, sns.color_palette().as_hex()[0])
 plt.plot(x, -y, sns.color_palette().as_hex()[0])
 plt.xlim(-1.5, 1.5)
 plt.ylim(-1.5, 1.5)
+plt.show()
+
+x1 = np.linspace(-3, 3, 100000)
+y1 = 2*np.sqrt(1-((x1/3)**2))
+plt.plot(x, y, sns.color_palette().as_hex()[0])
+plt.plot(x, -y, sns.color_palette().as_hex()[0])
+plt.plot(x1, y1, sns.color_palette().as_hex()[1])
+plt.plot(x1, -y1, sns.color_palette().as_hex()[1])
+plt.xlim(-4, 4)
+plt.ylim(-4, 4)
 plt.show()
