bb95d025db83985c3ca624237eda06fd8081cc59
Emmett Shang <emmett.shang@gmail.com>
2019-05-22 21:41:02 +1000

    def function

---
+++ sandbox/introduction_to_functions.py
@@ -0,0 +1,18 @@
+def add(a, b):
+    mysum = a + b
+    return mysum
+
+sum_of_num = add(6, 14)
+print(sum_of_num)
+
+def substract(a, b):
+    mydifference = a - b
+    return mydifference
+difference_of_num = substract(24, 6)
+print(difference_of_num)
+
+def multiply(a, b):
+    myproduct = a * b
+    return myproduct
+product_of_num = multiply(13, 12)
+print(product_of_num)
