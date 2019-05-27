1df9cb1b11a15febdbdc07cdf397de8c13f7e774
Emmett Shang <emmett.shang@gmail.com>
2019-05-27 21:40:36 +1000

    extra characters

--- sandbox/writing_a_simple_function.py
+++ sandbox/writing_a_simple_function.py
@@ -6,11 +6,13 @@ def intersect(s1, s2):
             res.append(i)
     return res
 
+
+result = intersect([1,2,3,4,5], [3,4,5,6,7])
 print(intersect([1,2,3,4,5], [3,4,5,6,7]))
 
 def password(length):
     pw = str()
-    characters = "abcdefghijklmnopqrstuv" + "0123456789"
+    characters = "abcdefghijklmnopqrstuvwxyz" + "0123456789"
     for i in range(length):
         pw = pw + random.choice(characters)
     return pw
