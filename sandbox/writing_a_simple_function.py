5cbb0da63f79fac8c079abf6e7238898677b06b4
Emmett Shang <emmett.shang@gmail.com>
2019-05-29 17:49:02 +1000

    added function

--- sandbox/writing_a_simple_function.py
+++ sandbox/writing_a_simple_function.py
@@ -1,4 +1,6 @@
 import random
+
+
 def intersect(s1, s2):
     res = []
     for i in s1:
@@ -7,8 +9,9 @@ def intersect(s1, s2):
     return res
 
 
-result = intersect([1,2,3,4,5], [3,4,5,6,7])
-print(intersect([1,2,3,4,5], [3,4,5,6,7]))
+result = intersect([1, 2, 3, 4, 5], [3, 4, 5, 6, 7])
+print(intersect([1, 2, 3, 4, 5], [3, 4, 5, 6, 7]))
+
 
 def password(length):
     pw = str()
@@ -16,7 +19,19 @@ def password(length):
     for i in range(length):
         pw = pw + random.choice(characters)
     return pw
+
+
 print(password(4))
 print(password(8))
 print(password(10))
 print(password(50))
+
+
+def rsum(n):
+    rsum = 0
+    for k in range(n):
+        rsum += k
+    return rsum
+
+
+print(rsum(12))
