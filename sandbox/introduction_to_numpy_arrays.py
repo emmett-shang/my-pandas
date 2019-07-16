efcb21c1508743c61f4cd1040236771d78d548e4
Emmett Shang <emmett.shang@gmail.com>
2019-07-16 10:57:11 +1000

    introduction to numpy functions and np arrays

---
+++ sandbox/introduction_to_numpy_arrays.py
@@ -0,0 +1,19 @@
+"""
+
+https://www.youtube.com/watch?v=MzMVGPhLjDM
+"""
+
+import numpy as np
+
+zero_vector = np.zeros(5)
+zero_matrix = np.zeros((5, 3))
+print(zero_vector)
+print(zero_matrix)
+x = np.array([1, 2, 3])
+y = np.array([2, 4, 6])
+print(x)
+print(y)
+Matrix= np.array([[1, 3], [5, 9]])
+print(Matrix)
+A = Matrix.transpose()
+print(A)
