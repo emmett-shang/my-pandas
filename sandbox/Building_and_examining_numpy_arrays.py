ca5661f50055922626a25aeae574582189466833
Emmett Shang <emmett.shang@gmail.com>
2019-10-22 18:20:19 +1100

    spacing arrays equally, size and shape of arrays and conditions

---
+++ sandbox/Building_and_examining_numpy_arrays.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+x = np.linspace(0, 100, 10)
+print(x)
+
+y = np.logspace(1, 2, 10) # The integers must be the log of a certain number
+print(y)
+
+a = np.logspace(np.log10(250), np.log10(500), 10)
+print(a)
+
+X = np.array([[1,2,3], [4,5,6]])
+print(X.shape)
+print(X.size)
+
+Y = np.random.random(10)
+print(np.any(x > 0.9))
+print(np.all(x >= 0.1))
+print(Y)
